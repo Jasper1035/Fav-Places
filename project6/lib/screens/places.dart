e4c0b3ae28c07302699983500a577695f64d0c52
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-05-08 21:26:45 +0100

    new project setup

---
+++ project6/lib/screens/places.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:project6/widgets/places_list.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
+
+      body: const PlacesList(places: []),
+    );
+  }
+}
