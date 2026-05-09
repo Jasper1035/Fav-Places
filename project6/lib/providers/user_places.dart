e1d48c1ed712424b2cff378bd6cd42d31ef692d4
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-05-09 20:54:06 +0100

    implement the riverpod provider

---
+++ project6/lib/providers/user_places.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:project6/models/place.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [...state, newPlace];
+  }
+}
+
+final userPlacesProvider = StateNotifierProvider((ref) => UserPlacesNotifier());
