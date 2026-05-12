434dd6e45ea4daf8df6e5b9b19751a47030fe5aa
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-05-12 21:32:46 +0100

    add the detials

---
+++ project6/lib/screens/places_details.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:project6/screens/places.dart';
+
+class PlacesDetailsScreen extends StatelessWidget {
+  PlacesDetailsScreen({super.key, required this.place});
+
+  final PlacesScreen place;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
