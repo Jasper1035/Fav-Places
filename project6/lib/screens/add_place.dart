695fae2826f07863db05143880ab28aa052bc37d
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-05-16 22:01:23 +0100

    add the location feature
    add the location api and location package to get the current location. send the req over the http network.

--- project6/lib/screens/add_place.dart
+++ project6/lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project6/models/place.dart';
 import 'package:project6/providers/user_places.dart';
 import 'package:project6/widgets/image_input.dart';
 import 'package:project6/widgets/location_input.dart';
@@ -17,16 +18,20 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
 
   File? _selectedImage;
 
+  PlaceLocation? _slectedLocation;
+
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty || _selectedImage == null) {
+    if (enteredTitle.isEmpty ||
+        _selectedImage == null ||
+        _slectedLocation == null) {
       return;
     }
 
     ref
         .read(userPlacesProvider.notifier)
-        .addPlace(enteredTitle, _selectedImage!);
+        .addPlace(enteredTitle, _selectedImage!, _slectedLocation!);
     Navigator.of(context).pop();
   }
 
@@ -57,7 +62,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               },
             ),
             const SizedBox(height: 10),
-            const LocationInput(),
+            LocationInput(
+              onSelectLocation: (location) {
+                _slectedLocation = location;
+              },
+            ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
